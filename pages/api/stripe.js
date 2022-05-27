733d5742caba7d07e306158e9b79dfc3c0c5b49d
Delia Paternina <delia.paternina@gmail.com>
2022-05-27 09:41:12 +0100

    fixes footer overflow

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -16,6 +16,7 @@ export default async function handler(req, res) {
         line_items: req.body.map((item) => {
           const img = item.image[0].asset._ref;
           const newImage = img.replace('image-', 'https://cdn.sanity.io/images/z6ofcoav/production/').replace('-webp', '.webp');
+          console.log(newImage);
 
           return {
             price_data: { 
